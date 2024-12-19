52566bdc87c099e5585d5ed4dd68ead7b507fea1
Arthur Reis <110342375+arthuzao22@users.noreply.github.com>
2024-12-19 08:44:51 -0300

    Feat: Atualização do banco de dados

--- systemestoque/settings.py
+++ systemestoque/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'systemestoque.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',  # Nome do banco de dados
-        'USER': 'postgres',  # Usuário
-        'PASSWORD': '0iCvDs3KpSqQiOLU',  # Senha
-        'HOST': 'imprudently-savvy-zander.data-1.use1.tembo.io',  # Hostname do servidor
-        'PORT': '5432',  # Porta do banco de dados
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'postgres',  # Nome do banco de dados
+#        'USER': 'postgres',  # Usuário
+#        'PASSWORD': '0iCvDs3KpSqQiOLU',  # Senha
+#        'HOST': 'imprudently-savvy-zander.data-1.use1.tembo.io',  # Hostname do servidor
+#        'PORT': '5432',  # Porta do banco de dados
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -128,7 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'PT-BR'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
